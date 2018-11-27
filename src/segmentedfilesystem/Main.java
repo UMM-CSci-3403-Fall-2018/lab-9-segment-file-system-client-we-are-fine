e6935b3fac5f0ef534b8d1b85f0d797a0323fafd
stev0531 <stev0531@morris.umn.edu>
2018-11-27 11:25:52 -0600

    Start working on client
    Attempted to create datagram sockets and
    packets for communicating with the server.
    Added a buffer used by the datagrams, and
    they are being sent and received by the client.
    We also made an arraylist to store the packet information
    received. Not currently compiling.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,32 @@
 package segmentedfilesystem;
 
+import Java.util.ArrayList;
+import Java.net.DatagramPacket;
+import Java.net.DatagramSocket;
+
 public class Main {
     
     public static void main(String[] args) {
-        
+        byte[] buf = new byte[256];
+        int port = 6014;
+        InetAddress address = InetAddress.getByName("heartofgold.morris.umn.edu");
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+
+        ArrayList heap = new ArrayList();
+
+        // Sending the Datagram
+        socket.send(packet);
+
+        // Getting a response back
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+        //DatagramPacket received = new (packet.getData(), 0, packet.getLength());
+        byte[] received = new byte[packet.getLength()];
+        received = packet.getData();
+
+        heap.add(received);
+
     }
 
 }
