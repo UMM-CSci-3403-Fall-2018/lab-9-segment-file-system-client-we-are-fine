75c630199cad3379b72f7123694606658a64adb5
rabex028 <rabex028@morris.umn.edu>
2018-11-29 11:36:18 -0600

    Create functions to return number of packets in file
    Created getPacketNumber, which takes a byte[]
    and returns the packet number of the file.
    Also created totalPackets, which takes the last
    packets of each file and returns the total number
    of packets in each.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -7,30 +7,38 @@ import java.net.InetAddress;
 import java.io.*;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 
 public class Main {
     
     public static void main(String[] args) {
         try {
-            byte[] buf = new byte[256];
+            byte[] buf = new byte[1028];
             int port = 6014;
             InetAddress address = InetAddress.getByName("heartofgold.morris.umn.edu");
             DatagramSocket socket = new DatagramSocket();
             DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
 
-            ArrayList heap = new ArrayList();
+            ArrayList<byte[]> heap = new ArrayList<byte[]>();
+            ArrayList<byte[]> lastPackets = new ArrayList<byte[]>();
 
             // Sending the Datagram
             socket.send(packet);
 
             // Getting a response back
-            packet = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
-            //DatagramPacket received = new (packet.getData(), 0, packet.getLength());
-            byte[] received = new byte[packet.getLength()];
-            received = packet.getData();
+           //while (lastPackets.size() < 3 && heap.size() < totalPackets(lastPackets)) {
 
-            heap.add(received);
+               packet = new DatagramPacket(buf, buf.length);
+               socket.receive(packet);
+               byte[] received = new byte[packet.getLength()];
+               received = packet.getData();
+               heap.add(received);
+
+               // if(received[0] mod)
+
+               getPacketNumber(received);
+
+           //}
 
         } catch(SocketException se) {
 
@@ -45,5 +53,29 @@ public class Main {
             System.out.println(ioe);
         }
     }
+
+    public static int totalPackets(ArrayList lastPackets){
+        int totalPackets = 0;
+
+//        for(int i = 0; i < lastPackets.size(); i++) {
+//            totalPackets += getPacketNumber(lastPackets.get(i));
+//        }
+
+        return totalPackets + 6; //adding 6 since packet numbers start from 0 and don't include the header packets
+    }
+
+
+    public static int getPacketNumber(byte[] b){
+
+        if (b[3] < 0) {
+            b[3] += 256;
+        }
+        int packetNumber = 256*b[2]+b[3];
+
+        System.out.println(packetNumber);
+
+        return packetNumber;
+    }
+
 
 }
