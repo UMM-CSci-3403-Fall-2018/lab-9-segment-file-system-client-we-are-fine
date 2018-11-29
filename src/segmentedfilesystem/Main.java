1d3ca16bec381cbbd89794eea504902b6d7a0c0b
stev0531 <stev0531@morris.umn.edu>
2018-11-29 10:16:56 -0600

    Implement try catches
    Added try/catch blocks to mitigate
    compilation errors. Fixed import
    capitalization.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,32 +1,49 @@
 package segmentedfilesystem;
 
-import Java.util.ArrayList;
-import Java.net.DatagramPacket;
-import Java.net.DatagramSocket;
+import java.util.ArrayList;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.io.*;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Main {
     
     public static void main(String[] args) {
-        byte[] buf = new byte[256];
-        int port = 6014;
-        InetAddress address = InetAddress.getByName("heartofgold.morris.umn.edu");
-        DatagramSocket socket = new DatagramSocket();
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+        try {
+            byte[] buf = new byte[256];
+            int port = 6014;
+            InetAddress address = InetAddress.getByName("heartofgold.morris.umn.edu");
+            DatagramSocket socket = new DatagramSocket();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
 
-        ArrayList heap = new ArrayList();
+            ArrayList heap = new ArrayList();
 
-        // Sending the Datagram
-        socket.send(packet);
+            // Sending the Datagram
+            socket.send(packet);
 
-        // Getting a response back
-        packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
-        //DatagramPacket received = new (packet.getData(), 0, packet.getLength());
-        byte[] received = new byte[packet.getLength()];
-        received = packet.getData();
+            // Getting a response back
+            packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+            //DatagramPacket received = new (packet.getData(), 0, packet.getLength());
+            byte[] received = new byte[packet.getLength()];
+            received = packet.getData();
 
-        heap.add(received);
+            heap.add(received);
 
+        } catch(SocketException se) {
+
+            System.out.println(se);
+
+        } catch(UnknownHostException uhe) {
+
+            System.out.println(uhe);
+
+        } catch(IOException ioe) {
+
+            System.out.println(ioe);
+        }
     }
 
 }
