3ae4ba7a16daedba23b05b0773e6b4882f66a761
rabex028 <rabex028@morris.umn.edu>
2018-12-04 10:04:43 -0600

    Implement checking packet types and fix getting packet numbers
    A new function that checks the type of the packet before it is
    placed in its proper list has been added. In addition, integers are being used during the packet number retrieval operations to give accurate results. Moved lists for packet storage outside of main method. While loop for receiving packets from server is also now being used.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -9,8 +9,13 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 
+
 public class Main {
-    
+
+    static ArrayList<byte[]> heap = new ArrayList<byte[]>();
+    static ArrayList<byte[]> lastPackets = new ArrayList<byte[]>();
+    static ArrayList<byte[]> headerPackets = new ArrayList<byte[]>();
+
     public static void main(String[] args) {
         try {
             byte[] buf = new byte[1028];
@@ -19,63 +24,85 @@ public class Main {
             DatagramSocket socket = new DatagramSocket();
             DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
 
-            ArrayList<byte[]> heap = new ArrayList<byte[]>();
-            ArrayList<byte[]> lastPackets = new ArrayList<byte[]>();
 
             // Sending the Datagram
             socket.send(packet);
 
             // Getting a response back
-           //while (lastPackets.size() < 3 && heap.size() < totalPackets(lastPackets)) {
+            while ((lastPackets.size() < 3) || (heap.size() < totalPackets(lastPackets))) {
 
-               packet = new DatagramPacket(buf, buf.length);
-               socket.receive(packet);
-               byte[] received = new byte[packet.getLength()];
-               received = packet.getData();
-               heap.add(received);
+                packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                byte[] received;
+                received = packet.getData();
 
-               // if(received[0] mod)
+                checkPacketType(received);
 
-               getPacketNumber(received);
 
-           //}
+            }
+            System.out.println(lastPackets.size());
+            System.out.println(headerPackets.size());
+            System.out.println(heap.size());
 
-        } catch(SocketException se) {
+
+        } catch (SocketException se) {
 
             System.out.println(se);
 
-        } catch(UnknownHostException uhe) {
+        } catch (UnknownHostException uhe) {
 
             System.out.println(uhe);
 
-        } catch(IOException ioe) {
+        } catch (IOException ioe) {
 
             System.out.println(ioe);
         }
     }
 
-    public static int totalPackets(ArrayList lastPackets){
+    public static int totalPackets(ArrayList<byte[]> lastPackets) {
         int totalPackets = 0;
 
-//        for(int i = 0; i < lastPackets.size(); i++) {
-//            totalPackets += getPacketNumber(lastPackets.get(i));
-//        }
-
-        return totalPackets + 6; //adding 6 since packet numbers start from 0 and don't include the header packets
-    }
-
-
-    public static int getPacketNumber(byte[] b){
-
-        if (b[3] < 0) {
-            b[3] += 256;
+        for (int i = 0; i < lastPackets.size(); i++) {
+            totalPackets += getPacketNumber(lastPackets.get(i));
         }
-        int packetNumber = 256*b[2]+b[3];
 
-        System.out.println(packetNumber);
+        System.out.println("Total Packets: " + (totalPackets + lastPackets.size()));
 
-        return packetNumber;
+        return totalPackets;
     }
 
 
+    public static int getPacketNumber(byte[] received) {
+
+        int packetByte1 = received[2];
+        int packetByte2 = received[3];
+
+        if (packetByte2 < 0) {
+            packetByte2 += 256;
+        }
+
+        int packetNumber = 256 * packetByte1 + packetByte2;
+
+        System.out.println("packet number: " + packetNumber);
+
+        return packetNumber + 1; // The packet count starts at 0, so 1 is added
+    }
+
+
+    public static void checkPacketType(byte[] received) {
+
+        if (received[0] % 2 == 1) {
+
+            if (received[0] % 4 == 3) {
+                lastPackets.add(received);
+                heap.add(received);
+
+            } else {
+                heap.add(received);
+            }
+        } else {
+            System.out.println("This should be a header packet");
+            headerPackets.add(received);
+        }
+    }
 }
